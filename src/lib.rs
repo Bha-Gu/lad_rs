d91ee0f85d2159c607dda30a7f5f626f41b488cb
Bha-Gu <bhaumik050402@gmail.com>
2024-10-14 12:30:33 +0530

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 pub mod binarization;
 
+pub use crate::binarization::{binarize::Binarizer, rule_generation::RuleGenerator};
+
 //#[pymodule]
 //pub fn lad_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
 //    m.add_class::<Binarizer>()?;
